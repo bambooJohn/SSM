e1fa09191e41cba89da6d6c1bbeb2417d73f4d64
liangzhen <1033855573@qq.com>
2021-01-12 16:11:25 +0800

    05-spring配置介绍

--- day01_Spring/src/com/bambooJohn/spring/mod/TestBySpring.java
+++ day01_Spring/src/com/bambooJohn/spring/mod/TestBySpring.java
@@ -10,7 +10,10 @@ public class TestBySpring {
 		//初始化容器
 		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
 		//通过getBean()获取对象
-		Person bean = (Person) ac.getBean("person");
+		//Person bean = (Person) ac.getBean("person");
+		//使用此方法获取对象时，要求spring所管理的此类型的对象只能有一个
+		//Person bean = ac.getBean(Person.class);
+		Person bean = ac.getBean("personTwo",Person.class);
 		System.out.println(bean);
 		
 		
