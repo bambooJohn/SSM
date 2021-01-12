7628bd71e158a3944cdd02c7a941051f83cf168a
liangzhen <1033855573@qq.com>
2021-01-12 19:34:26 +0800

    06-注入方式

---
+++ day01_Spring/src/com/bambooJohn/spring/di/Test.java
@@ -0,0 +1,20 @@
+package com.bambooJohn.spring.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans-di.xml");
+		Student student = ac.getBean("s1", Student.class);
+		System.out.println(student);
+		Student student2 = ac.getBean("s2", Student.class);
+		System.out.println(student2);
+		Student student3 = ac.getBean("s3", Student.class);
+		System.out.println(student3);
+	}
+
+}
