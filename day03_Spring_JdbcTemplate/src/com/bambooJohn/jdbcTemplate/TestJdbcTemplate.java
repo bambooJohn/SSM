4db0a244c380673f5e2398d7dc2603d5459e6250
liangzhen <1033855573@qq.com>
2021-01-23 20:59:26 +0800

    16-JdbcTemplate之batchUpdate（批量增删改）

--- day03_Spring_JdbcTemplate/src/com/bambooJohn/jdbcTemplate/TestJdbcTemplate.java
+++ day03_Spring_JdbcTemplate/src/com/bambooJohn/jdbcTemplate/TestJdbcTemplate.java
@@ -2,6 +2,9 @@ package com.bambooJohn.jdbcTemplate;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,11 +16,31 @@ public class TestJdbcTemplate {
 	JdbcTemplate jdbcTemplate = ac.getBean("jdbcTemplate", JdbcTemplate.class);
 	
 	@Test
-	public void test() {
+	public void testUpdate() {
 
 		//jdbcTemplate.update("insert into emp values(null,'张三',23,'男')");
+		/*String sql = "insert into emp values(null,?,?,?)";
+		jdbcTemplate.update(sql,"李四",24,"女");*/
+		
+		/*String eids  = "3,4,5";
+		String sql = "delete from emp where eid in (" + eids + ")";
+		jdbcTemplate.update(sql);*/
+		
+		String mohu = "a";
+		String sql = "select * from emp where ename like '%?%'"; // 不能使用
+		String sqlss = "select * from emp where ename like concat('%',?,'%')"; // 可以使用
+		
+	}
+	
+	@Test
+	public void testBatchUpdate() {
+		
 		String sql = "insert into emp values(null,?,?,?)";
-		jdbcTemplate.update(sql,"李四",24,"女");
+		List<Object[]> list = new ArrayList<Object[]> ();
+		list.add(new Object[] {"a1",1,"男"});
+		list.add(new Object[] {"a2",2,"男"});
+		list.add(new Object[] {"a3",3,"男"});
+		jdbcTemplate.batchUpdate(sql, list); // 批量增删改
 	}
 
 }
