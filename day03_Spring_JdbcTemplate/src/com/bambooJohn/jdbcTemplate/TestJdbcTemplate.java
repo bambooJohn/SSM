887e1a04a7906aac7f4ae592641103fbdbaa4d9b
liangzhen <1033855573@qq.com>
2021-01-23 22:57:02 +0800

    17-JdbcTemplate之queryForObject

--- day03_Spring_JdbcTemplate/src/com/bambooJohn/jdbcTemplate/TestJdbcTemplate.java
+++ day03_Spring_JdbcTemplate/src/com/bambooJohn/jdbcTemplate/TestJdbcTemplate.java
@@ -8,7 +8,9 @@ import java.util.List;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class TestJdbcTemplate {
 
@@ -43,4 +45,20 @@ public class TestJdbcTemplate {
 		jdbcTemplate.batchUpdate(sql, list); // 批量增删改
 	}
 
+	@Test
+	public void testQueryForObject() {
+		//jdbcTemplate.queryForObject(sql, requiredType) // 用来获取单个的值
+		//jdbcTemplate.queryForObject(sql, rowMapper) // 用来获取单条数据
+		
+		String sql = "select eid,ename,age,sex from emp where eid = ?";
+		
+		RowMapper<Emp> rowMapper = new BeanPropertyRowMapper<>(Emp.class);
+		Emp emp = jdbcTemplate.queryForObject(sql, new Object[] {7}, rowMapper);
+		System.out.println(emp);
+		
+		String sql2 = "select count(*) from emp";
+		Integer count = jdbcTemplate.queryForObject(sql2, Integer.class);
+		System.out.println(count);
+	}
+	
 }
