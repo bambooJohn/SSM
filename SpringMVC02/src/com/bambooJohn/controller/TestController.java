9694d52379679d9043c0f3ea3cdc4ba0f79801ec
liangzhen <1033855573@qq.com>
2021-01-29 22:33:40 +0800

    06-springMVC支持占位符方式的路径

--- SpringMVC02/src/com/bambooJohn/controller/TestController.java
+++ SpringMVC02/src/com/bambooJohn/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.bambooJohn.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -57,6 +58,17 @@ public class TestController {
 	public String testAnt() {
 		
 		System.out.println("SUCCESS:testAnt");
+		
+		return "success";
+		
+	}
+	
+	
+	@RequestMapping(value="/testREST/{id}/{username}")
+	public String testREST(@PathVariable("id") Integer id,@PathVariable("username") String username) {
+		
+		System.out.println("SUCCESS:testREST");
+		System.out.println(id + "=====" + username);
 		
 		return "success";
 		
