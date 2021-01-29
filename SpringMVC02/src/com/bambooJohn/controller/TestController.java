3b21314bc578eaa8594a86278c51b6ecbe0198df
liangzhen <1033855573@qq.com>
2021-01-29 12:06:25 +0800

    04-@RequestMapping作用位置

--- SpringMVC02/src/com/bambooJohn/controller/TestController.java
+++ SpringMVC02/src/com/bambooJohn/controller/TestController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/mvc")
 public class TestController {
 
 	/**
 	 * @RequestMapping:设置请求映射，把请求和控制层中的方法设置映射关系
 	 * 当请求路径和@RequestMapping的value属性值一致时，则该注解所标注的方法即为处理请求的方法
+	 * 
+	 * @RequestMapping可以加在类上，也可以加在方法上
+	 * 若类和方法上都加的有，应该一层一层的访问，先访问类，再访问类中的方法
+	 * 
 	 * method:用来设置请求方式，只有客户端发送请求的方式和method的值一致，才能处理请求
 	 * 请求方式：GET 查询  POST 添加 PUT 修改 DELETE 删除
 	 * params:用来设置客户端传到服务器端的数据，支持表达式
