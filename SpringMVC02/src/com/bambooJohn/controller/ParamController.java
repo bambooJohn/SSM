4c0a383711d6ee566d2173f5782abaa7cda902e0
liangzhen <1033855573@qq.com>
2021-01-31 18:18:41 +0800

    09-springMVC获取客户端数据

---
+++ SpringMVC02/src/com/bambooJohn/controller/ParamController.java
@@ -0,0 +1,29 @@
+package com.bambooJohn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ParamController {
+
+	/**
+	 * 在springMVC获取客户端传递的数据的方式：
+	 * 1、在处理请求的方法中，加入相应的形参，保证形参参数名和传递的数据的参数名保持一致，就可以自动赋值
+	 * value:当不满足赋值条件时，可以使用value属性，指定映射关系
+	 * required:设置形参是否必须被赋值，默认为true，必须赋值，若设置为false，则不必须赋值，因此形参的值为null
+	 * defaultValue:若形参所获得的值为null,则设置一个默认值，用在分页和模糊查询中
+	 * @param username
+	 * @param password
+	 * @param age
+	 * @return
+	 */
+	@RequestMapping(value="/testParam",method=RequestMethod.POST)
+	public String testParam(@RequestParam(value="name",required=false,defaultValue="admin")String username,String password,Integer age) {
+		
+		System.out.println("username=" + username + ", password=" + password + ", age=" + age);
+		return "success";
+	}
+	
+}
