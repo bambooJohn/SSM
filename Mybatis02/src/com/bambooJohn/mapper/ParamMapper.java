0cd4c622ce585c3c3af294d4be9f64ec8adf66c3
liangzhen <1033855573@qq.com>
2021-02-25 20:05:07 +0800

    10-mybatis获取不同参数值的方式1

--- Mybatis02/src/com/bambooJohn/mapper/ParamMapper.java
+++ Mybatis02/src/com/bambooJohn/mapper/ParamMapper.java
@@ -1,9 +1,17 @@
 package com.bambooJohn.mapper;
 
+import java.util.Map;
+
 import com.bambooJohn.bean.Emp;
 
 public interface ParamMapper {
 
 	void insertEmp(Emp emp);
+	
+	Emp getEmpByEid(String eid);
+	
+	Emp getEmpByEidAndEname(String eid,String ename);
+	
+	Emp getEmpByMap(Map<String,Object> map);
 	
 }
--- Mybatis02/src/com/bambooJohn/test/TestParam.java
+++ Mybatis02/src/com/bambooJohn/test/TestParam.java
@@ -4,7 +4,9 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -25,9 +27,21 @@ public class TestParam {
 		//SqlSession sqlSession = sqlSessionFactory.openSession(); // 需要手动处理事务
 		SqlSession sqlSession = sqlSessionFactory.openSession(true); // 自动处理事务
 		ParamMapper paramMapper = sqlSession.getMapper(ParamMapper.class);
-		Emp emp = new Emp(null, "赵洞庭", 23, "男");
+		/*Emp emp = new Emp(null, "赵洞庭", 23, "男");
 		paramMapper.insertEmp(emp);
-		System.out.println(emp.getEid()); // 获取主键eid
+		System.out.println(emp.getEid()); */// 获取主键eid
+		
+		Emp emp = paramMapper.getEmpByEid("1");
+		System.out.println(emp);
+		
+		Emp eidAndEname = paramMapper.getEmpByEidAndEname("2", "李四");
+		System.out.println(eidAndEname);
+		
+		Map<String,Object> map = new HashMap<>();
+		map.put("eid","6");
+		map.put("ename","a1");
+		Emp empByMap = paramMapper.getEmpByMap(map);
+		System.out.println(empByMap);
 		
 	}
 
