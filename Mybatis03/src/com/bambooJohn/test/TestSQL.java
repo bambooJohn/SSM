38aebe06cc25a93715e2341e529ac58b16dd8bae
liangzhen <1033855573@qq.com>
2021-02-27 17:27:17 +0800

    05动态SQL之trim

--- Mybatis03/src/com/bambooJohn/test/TestSQL.java
+++ Mybatis03/src/com/bambooJohn/test/TestSQL.java
@@ -23,10 +23,10 @@ class TestSQL {
 		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 		EmpMapper empMapper = sqlSession.getMapper(EmpMapper.class);
 		Emp emp = new Emp();
-		emp.setEid(1);
+		//emp.setEid(1);
 		emp.setEname("李三");
-		//emp.setAge(23);
-		emp.setSex("1");
+		emp.setAge(23);
+		//emp.setSex("1");
 		List<Emp> empListByMoreTJ = empMapper.getEmpListByMoreTJ(emp);
 		System.out.println(empListByMoreTJ);
 		
